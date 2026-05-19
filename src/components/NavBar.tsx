0254f3cc6caf09b97800e6003c31a21775993162
Sosa Sofia <80438887+Sosasofia@users.noreply.github.com>
2026-05-19 11:42:59 -0300

    fix(navbar): simplify sign-in button rendering based on user authentication

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -34,16 +34,7 @@ export default async function NavBar() {
             </button>
           </div>
           <div>
-            {user ? (
-              <SignOutButton />
-            ) : (
-              <Link
-                href={"/sign-in"}
-                className="bg-blue-500 ml-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-              >
-                Sign In
-              </Link>
-            )}
+            {user ? <SignOutButton /> : null}
           </div>
         </div>
       </header>
